9d2606326451538779561a3fd7ed474557e08fea
mkramar <mike.kramarenko@gmail.com>
2018-12-15 22:25:10 +1100

    current work

---
+++ Electronics/Motor/Firmware/Status.cpp
@@ -0,0 +1,51 @@
+#include <main.h>
+
+volatile bool buttonIdPressed;
+volatile bool buttonCalibPressed;
+volatile int buttonPressId = 0;
+
+const int BLINK_PERIOD = 0x100;
+const int BLINK_DUTY_CYCLE = 0x40;
+const int BLINK_PRESCALER = 0x6000;
+
+void stopIdTimer()
+{
+	TIM2->CR1 &= ~TIM_CR1_CEN;							// disable timer 2
+	GPIOA->MODER &= ~GPIO_MODER_MODER0_Msk;
+	GPIOA->MODER |= (0x01 << GPIO_MODER_MODER0_Pos);	// output mode for A-0
+	GPIOA->BSRR |= 0x01;								// set pin A-0	
+}
+
+
+void blinkId(bool onOff) {
+	if (onOff)
+	{
+		// PA-0 configure as led timer 2 blinker
+		
+		RCC->AHBENR |= RCC_AHBENR_GPIOAEN;					// enable clock for GPIOA
+		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;					// enable timer 2
+	
+		TIM2->ARR = BLINK_PERIOD;							// tim2 period
+		TIM2->PSC = BLINK_PRESCALER;						// prescaler
+
+		// tim2 config channel
+
+		TIM2->CCMR1 |= TIM_CCMR1_OC1PE |					// Output compare 1 preload enable
+					   (0x06 << TIM_CCMR1_OC1M_Pos);		// Output compare 1 mode = 110
+
+		// GPIOA
+	
+		GPIOA->MODER &= ~GPIO_MODER_MODER0_Msk;
+		GPIOA->MODER |= (0x02 << GPIO_MODER_MODER0_Pos);	// alternative function for pin A-0
+		GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR0;			// high speed for pin A-0
+		GPIOA->AFR[0] |= (0x02 << GPIO_AFRL_AFSEL0_Pos);	// alternative funciton 2 for pin A-0
+
+		TIM2->CCR1 = BLINK_DUTY_CYCLE;						// duty cycle of tim2 channel 1
+		TIM2->CCER |= TIM_CCER_CC1E;						// enable tim2 channel 1, positive
+		TIM2->CR1 |= TIM_CR1_CEN;							// enable timer 2
+	}
+	else
+	{
+		stopIdTimer();
+	}
+}
---
+++ Electronics/Motor/Firmware/Temperature.cpp
@@ -0,0 +1,55 @@
+#include <main.h>
+#include <stm32f0xx_ll_adc.h>
+
+const uint32_t TEMP_PERIOD = 1000;
+const uint32_t TEMP_PRESCALER = 48000;
+
+#define TEMP110_CAL_ADDR TEMPSENSOR_CAL2_ADDR
+#define TEMP30_CAL_ADDR TEMPSENSOR_CAL1_ADDR
+
+int32_t temperature; // temperature in degrees Celsius
+
+extern "C"
+void TIM3_IRQHandler(void) {
+	TIM3->SR &= ~TIM_SR_UIF;
+	
+	// temp
+	
+	int sensor = ADC1->DR;
+	int calib30 = (int) *TEMP30_CAL_ADDR;
+	int calib110 = (int) *TEMP110_CAL_ADDR;
+	
+	temperature = (sensor - calib30) * (110 - 30) / (calib110 - calib30) + 30;
+}
+
+void initTemperature(){
+	// timer
+	
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;					// enable timer 3
+	
+	TIM3->ARR = TEMP_PERIOD;							// tim3 period
+	TIM3->PSC = TEMP_PRESCALER;							// prescaler
+
+	TIM3->DIER |= TIM_DIER_UIE;							// enable update interrupt
+	TIM3->CR1 |= TIM_CR1_CEN;							// enable timer 3	
+	
+	HAL_NVIC_SetPriority(TIM3_IRQn, 1, 1);
+	HAL_NVIC_EnableIRQ(TIM3_IRQn);
+	
+	// ADC
+	
+	RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;					// enable ADC1
+	
+	ADC1->CHSELR = ADC_CHSELR_CHSEL16;					//  Select CHSEL16 for temperature sensor
+	ADC1->SMPR |= ADC_SMPR_SMP_0 | ADC_SMPR_SMP_1 | ADC_SMPR_SMP_2;	// sample rate
+	ADC->CCR |= ADC_CCR_TSEN;							// enable temp sensor
+	ADC->CCR |= ADC_CCR_VREFEN;							// enable internal voltage reference
+	
+	ADC1->CFGR1 |= ADC_CFGR1_CONT;						// continous mode 
+		
+	ADC1->CR |= ADC_CR_ADEN;							// enable ADC
+	while ((ADC1->ISR & ADC_ISR_ADRDY) == 0) {}			// wait for ADC to be ready
+		
+	ADC1->CR |= ADC_CR_ADSTART;
+}
